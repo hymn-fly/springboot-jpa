6ab09c3fc0a2fd677eac7b2b938880750e8bb8b3
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-12 14:23:52 +0900

    수정  - application.yaml 수정 for auto-config  - DataSourceConfig remove @Configuration

--- springboot-jpa/src/main/java/com/example/springjpa/config/DataSourceConfig.java
+++ springboot-jpa/src/main/java/com/example/springjpa/config/DataSourceConfig.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-@Configuration
+//@Configuration
 public class DataSourceConfig {
 
     @Bean
