3f7a8a7f27bbe8fc5c96ed3ff3b1c5e94b79ed07
Gil KeunO <rlfrmsdh@nextlab.co.kr>
2022-05-13 15:31:44 +0900

    추가  - restdocs 적용해보기  - index.adoc을 통해 rest docs html 파일 만들기

--- springboot-jpa/src/test/java/com/example/springjpa/order/controller/OrderControllerTest.java
+++ springboot-jpa/src/test/java/com/example/springjpa/order/controller/OrderControllerTest.java
@@ -13,9 +13,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
@@ -23,11 +26,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @SpringBootTest
 class OrderControllerTest {
 
@@ -77,7 +84,39 @@ class OrderControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(orderDtoData)))
                 .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(print())
+                .andDo(document("order-save",
+                        requestFields(
+                                fieldWithPath("uuid").type(JsonFieldType.STRING).description("UUID"),
+                                fieldWithPath("orderDatetime").type(JsonFieldType.STRING).description("orderDatetime"),
+                                fieldWithPath("orderStatus").type(JsonFieldType.STRING).description("orderStatus"),
+                                fieldWithPath("memo").type(JsonFieldType.STRING).description("memo"),
+                                fieldWithPath("memberDto").type(JsonFieldType.OBJECT).description("memberDto"),
+                                fieldWithPath("memberDto.id").type(JsonFieldType.NUMBER).description("memberDto.id"),
+                                fieldWithPath("memberDto.name").type(JsonFieldType.STRING).description("memberDto.name"),
+                                fieldWithPath("memberDto.nickName").type(JsonFieldType.STRING).description("memberDto.nickName"),
+                                fieldWithPath("memberDto.age").type(JsonFieldType.NUMBER).description("memberDto.age"),
+                                fieldWithPath("memberDto.address").type(JsonFieldType.STRING).description("memberDto.address"),
+                                fieldWithPath("memberDto.description").type(JsonFieldType.STRING).description("memberDto.description"),
+                                fieldWithPath("orderItemDtos").type(JsonFieldType.ARRAY).description("orderItemDtos"),
+                                fieldWithPath("orderItemDtos[].id").type(JsonFieldType.NUMBER).description("orderItemDtos[].id"),
+                                fieldWithPath("orderItemDtos[].quantity").type(JsonFieldType.NUMBER).description("orderItemDtos[].quantity"),
+                                fieldWithPath("orderItemDtos[].price").type(JsonFieldType.NUMBER).description("orderItemDtos[].price"),
+                                fieldWithPath("orderItemDtos[].itemDto").type(JsonFieldType.OBJECT).description("orderItemDtos[].itemDto"),
+                                fieldWithPath("orderItemDtos[].itemDto.id").type(JsonFieldType.NUMBER).description("orderItemDtos[].itemDto.id"),
+                                fieldWithPath("orderItemDtos[].itemDto.type").type(JsonFieldType.STRING).description("orderItemDtos[].itemDto.type"),
+                                fieldWithPath("orderItemDtos[].itemDto.price").type(JsonFieldType.NUMBER).description("orderItemDtos[].itemDto.price"),
+                                fieldWithPath("orderItemDtos[].itemDto.stockQuantity").type(JsonFieldType.NUMBER).description("orderItemDtos[].itemDto.stockQuantity"),
+                                fieldWithPath("orderItemDtos[].itemDto.power").type(JsonFieldType.NUMBER).description("orderItemDtos[].itemDto.power"),
+                                fieldWithPath("orderItemDtos[].itemDto.chef").type(JsonFieldType.NULL).description("orderItemDtos[].itemDto.chef"),
+                                fieldWithPath("orderItemDtos[].itemDto.width").type(JsonFieldType.NUMBER).description("orderItemDtos[].itemDto.width"),
+                                fieldWithPath("orderItemDtos[].itemDto.height").type(JsonFieldType.NUMBER).description("orderItemDtos[].itemDto.height")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data").type(JsonFieldType.STRING).description("데이터"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("서버시간")
+                        )));
     }
 
     @Test
@@ -86,14 +125,14 @@ class OrderControllerTest {
                         .param("page", "0")
                         .param("size", "5"))
                 .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(print());
     }
 
     @Test
     void getOneTest() throws Exception{
         mvc.perform(get("/orders/{uuid}", orderDtoData.uuid()))
                 .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(print());
 
     }
 }
