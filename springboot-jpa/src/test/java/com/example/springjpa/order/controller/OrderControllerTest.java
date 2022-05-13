a96485015c570cd05a81021067a4d51fdba2c3e8
Gil KeunO <rlfrmsdh@nextlab.co.kr>
2022-05-13 15:43:24 +0900

    추가  - restdocs로 만든 html 파일

--- springboot-jpa/src/test/java/com/example/springjpa/order/controller/OrderControllerTest.java
+++ springboot-jpa/src/test/java/com/example/springjpa/order/controller/OrderControllerTest.java
@@ -17,10 +17,8 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.time.LocalDateTime;
 import java.util.List;
